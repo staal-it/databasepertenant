e54e09bd5d2033ffa8c46abc4f448d529e4b2025
Erwin Staal <info@staal-it.nl>
2021-03-19 14:32:46 +0100

    Added tenant models and context

---
+++ DatabasePerTenant.Data.Tenant/DatabasePerTenant.Data.Tenant/Models/BuildVersion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DatabasePerTenant.Data.Tenant.Models
+{
+    public partial class BuildVersion
+    {
+        public byte SystemInformationId { get; set; }
+        public string DatabaseVersion { get; set; }
+        public DateTime VersionDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
---
+++ DatabasePerTenant.Data.Tenant/DatabasePerTenant.Data.Tenant/Models/VGetAllCategories.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DatabasePerTenant.Data.Tenant.Models
+{
+    public partial class VGetAllCategories
+    {
+        public string ParentProductCategoryName { get; set; }
+        public string ProductCategoryName { get; set; }
+        public int? ProductCategoryId { get; set; }
+    }
+}
